f62e1d6562c8a9a446ddd4bd34eba184113e5c6e
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-12 14:44:23 +0500

    4.2. Результат работы метода [#232622]

--- src/main/java/ru/job4j/calculator/MathFunc.java
+++ src/main/java/ru/job4j/calculator/MathFunc.java
@@ -2,21 +2,21 @@ package ru.job4j.calculator;
 
 public class MathFunc {
 
-        public static int func1(int x) {
-        int y = x * x + 1;
+    public static float func1(float x) {
+        float y = x * x + 1;
         return y;
     }
 
-        public static int func2(int x) {
-        int y = x * 10;
+    public static float func2(float x) {
+        float y = 1 / x;
         return y;
     }
 
-        public static void main(String[] args) {
-        int result1 = MathFunc.func1(3);
-        int result2 = MathFunc.func2(5);
-        int result3 = MathFunc.func1(100);
-        int total = result3 + result2;
+    public static void main(String[] args) {
+        float result1 = MathFunc.func1(3);
+        float result2 = MathFunc.func2(5);
+        float result3 = MathFunc.func1(100);
+        float total = result3 + result2;
         System.out.println(total);
     }
 }
