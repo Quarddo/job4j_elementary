1290e2297eb2942d81998a0e2be7e8898511a2e0
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-21 19:30:59 +0500

    5.2. Создать программу, вычисляющую факториал. [#193]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -2,7 +2,7 @@ package ru.job4j.loop;
 
 public class Factorial {
     public static int calc(int n) {
-        int multi = 0;
+        int multi = 1;
         for (int index = 1; index <= n; index++) {
         multi = multi * index;
         }
--- src/test/java/ru/job4j/loop/FactorialTest.java
+++ src/test/java/ru/job4j/loop/FactorialTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.loop;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +12,7 @@ public class FactorialTest {
         int n = 5;
         int result = Factorial.calc(n);
         int expected = 120;
-
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -19,5 +20,6 @@ public class FactorialTest {
         int n = 0;
         int result = Factorial.calc(n);
         int expected = 1;
+        Assert.assertEquals(expected, result);
     }
 }
