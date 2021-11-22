e19a9b84309d06b37cf23f87580855364ba5f812
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-22 18:31:04 +0500

    5.2. Создать программу, вычисляющую факториал. [#193 #231246]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calc(int n) {
         int multi = 1;
-        for (int index = 1; index <= n; index++) {
-        multi = multi * index;
+        for (int index = 2; index <= n; index++) {
+        multi *= index;
         }
         return multi;
     }
