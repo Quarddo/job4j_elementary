8fbb52a0c7b1f5aea865f6e8124524a23e1a7033
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-26 18:12:31 +0500

    6.0.1. Объявление массива. [#3613]

---
+++ src/main/java/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surname = new String[100500];
+        float[] price = new float[40];
+    }
+}
