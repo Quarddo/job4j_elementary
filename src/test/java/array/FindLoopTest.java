c9b4a714ff1ddbcba1e696dd1a8e34197adf6b64
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-27 16:43:30 +0500

    6.1. Классический поиск перебором. [#33489]

---
+++ src/test/java/array/FindLoopTest.java
@@ -0,0 +1,36 @@
+package array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHas5Then0() {
+        int[] data = {5, 4, 3, 2};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas5Then5() {
+        int[] data = {5, 4, 3, 2};
+        int el = 4;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenArrayHas5ThenMinus1() {
+        int[] data = {5, 4, 3, 2};
+        int el = 7;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
