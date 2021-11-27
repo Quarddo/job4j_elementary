d131a694a2389415c698f490a90f9d11f58e69f2
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-27 14:55:45 +0500

    6.0.3. Массивы и цикл for. [#226665 #233097]

--- src/main/java/array/ArrayLoop.java
+++ src/main/java/array/ArrayLoop.java
@@ -2,12 +2,12 @@ package array;
 
 public class ArrayLoop {
     public static void main(String[] args) {
-        int[] y = new int[5];
-        for (int index = 0; index < y.length; index++) {
-            y[index] = index * 2 + 3;
+        int[] numbers = new int[5];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index * 2 + 3;
         }
-        for (int index = 0; index < y.length; index++) {
-            System.out.println(y[index]);
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
         }
 
     }
