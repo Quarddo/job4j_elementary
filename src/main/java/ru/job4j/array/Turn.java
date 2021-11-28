fb6bd84bb5ab283bd15ddb4242d7087757af0b4d
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-11-28 18:32:01 +0500

    6.2.3. Перевернуть массив. [#226790]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Turn {
+
+    public static int[] back(int[] array) {
+            int temp = array[0];
+            array[0] = array[array.length -1];
+            array[array.length -1] = temp;
+            temp = array[1];
+            array[1] = array[array.length -2];
+            array[array.length -2] = temp;
+        for (int i = 0; i < array.length; i++) {
+        }
+        return array;
+    }
+}
