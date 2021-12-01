b104ef9b5b63e20d1df56d6e401a29dec7380a3e
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-01 12:56:47 +0500

    6.4. Слово начинается с ... [#41585 #234048]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,14 +2,11 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = true;
-        for (int i = 0; i < word.length; i++) {
-            for (int j = 0; j < pref.length; j++) {
-                if (pref[1] != word[1]) {
+            for (int i = 0; i < pref.length; i++) {
+                if (pref[i] != word[i]) {
                     return false;
                 }
             }
-        }
         return true;
     }
 }
