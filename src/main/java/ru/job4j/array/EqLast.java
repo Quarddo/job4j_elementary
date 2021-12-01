3b97ec82bac6840b850941e29d5be255be522412
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-01 14:00:52 +0500

    6.4.1. Сравнить последние элементы двух массивов [#304228]

--- src/main/java/ru/job4j/array/EqLast.java
+++ src/main/java/ru/job4j/array/EqLast.java
@@ -2,11 +2,6 @@ package ru.job4j.array;
 
 public class EqLast {
     public static boolean check(int[] left, int[] right) {
-        for (int i = 0; i < right.length; i++) {
-            if (right[right.length - 1] != left[left.length - 1]) {
-                return false;
-            }
-        }
-        return true;
+        return right[right.length - 1] == left[left.length - 1];
     }
 }
