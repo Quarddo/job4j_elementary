b3a0200a0da5ce07757ad4911b48d3fbd64c7492
Quarddo <93788758+Quarddo@users.noreply.github.com>
2021-12-03 21:34:01 +0500

    6.6.3. Двухмерный массив. If. [#306727]

---
+++ src/test/java/ru/job4j/array/SkipNegative.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class SkipNegative {
+    public static int[][] skip(int[][] array) {
+        int rsl = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array[row].length; cell++) {
+                int val = array[row][cell];
+                if (array[row][cell] >= 0) {
+                }else{
+                    array[row][cell] = 0;
+                }
+            }
+        }
+        return array;
+    }
+}
