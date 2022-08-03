4f731fa5f03a073f447a52ca9a7c9d0ee76f1d8b
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-08-03 19:09:43 +0500

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+import static java.lang.Character.isDigit;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = false;
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (!name.isEmpty() && isLowerLatinLetter(name.codePointAt(0))
+            && (isSpecialSymbol(code) || isUpperLatinLetter(code)
+                    || isLowerLatinLetter(code) || isDigit(code))) {
+                valid = true;
+            }
+        }
+        return valid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
